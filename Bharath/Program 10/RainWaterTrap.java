09d7afb7c4abbaba4a905020228fc9223513c4a4
Bharath9583 <bharathkumar.kamal@gmail.com>
2017-03-13 22:30:40 +0530

    Program 10

---
+++ Bharath/Program 10/RainWaterTrap.java
@@ -0,0 +1,57 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+
+public class Solution {
+    
+	public static int rainWaterTrap(ArrayList<Integer> al){
+		int left=0; int right=al.size()-1;
+        int res=0;
+        int maxleft=0, maxright=0;
+        while(left<=right){
+            if(al.get(left)<=al.get(right)){
+                if(al.get(left)>=maxleft) maxleft=al.get(left);
+                else res+=maxleft-al.get(left);
+                left++;
+            }
+            else{
+                if(al.get(right)>=maxright) maxright= al.get(right);
+                else res+=maxright-al.get(right);
+                right--;
+            }
+        }
+        return res;
+    }
+    
+    public static int maxleftheight(int i,int[] arr){
+        int max = 0;
+        for(int k=0;k<i-1;k++){
+            if(max<arr[k]){
+                max = arr[k];
+            }
+        }
+        return max;
+    }
+    
+    public static int maxrightheight(int i,int[] arr){
+        int max = 0;
+        for(int k=i+1;k<arr.length;k++){
+            if(max<arr[k]){
+                max = arr[k];
+            }
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        ArrayList<Integer> al = new ArrayList<Integer>();
+        while(in.hasNextInt()){            
+        	al.add(in.nextInt());
+        }
+        in.close();
+        System.out.println(rainWaterTrap(al));
+        
+    }
+}
